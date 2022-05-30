13e513c4314fdf6d70f691ded49d38923ff4d9ca
Tim Daniel Friedrich <tim.daniel.friedrich@gmail.com>
2022-05-30 11:02:27 +0200

    commenting

--- PIC-Simulator/MainPage.xaml.cs
+++ PIC-Simulator/MainPage.xaml.cs
@@ -17,13 +17,9 @@ using System.Threading;
 
 
 
-// Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x407 dokumentiert.
 
 namespace PIC_Simulator
 {
-    /// <summary>
-    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
-    /// </summary>
     public sealed partial class MainPage : Page, ICodeInterface
     {
         Memory memory;
